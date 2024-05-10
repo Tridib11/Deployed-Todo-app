dcac4d67c6f9a20f08cdc7967a9c786033ddd31a
Tridib11 <tridibghosh12345@gmail.com>
2024-05-11 01:23:36 +0530

    chore: Configure server.js with route for homepage

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
 const PORT= process.env.PORT ??  8000;
 const express=require("express")
+const app=express()
 
+
+app.get("/",(req,res)=>{
+  res.send("hello tridib")
+})
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
