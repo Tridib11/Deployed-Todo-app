111b82f0be3cbea0a247438a7fbb3778e9cac5f0
Tridib11 <tridibghosh12345@gmail.com>
2024-05-11 04:48:32 +0530

    Files added

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -1,8 +1,20 @@
+import TickIcon  from "./TickIcon.jsx";
+import LinearProgress from '@mui/material/LinearProgress';
 function ListItem({task}) {
+    let progress=40;
     return (
-        <div>
-            <p>{task.title}</p>
-        </div>
+        <li className="list-item">
+            <div className="info-container">
+                <TickIcon/>
+                <p className="task-title">{task.title}</p>
+                <LinearProgress variant="determinate" value={progress} />
+            </div>
+
+            <div className="button-container">
+                <button className="edit">Edit</button>
+                <button className="delete">Delete</button>
+            </div>
+        </li>
     )
 }
 
