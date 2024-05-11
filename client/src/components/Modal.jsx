4fe9c64a5b7373f58e3d2c254c409d7d8f9a76f1
Tridib11 <tridibghosh12345@gmail.com>
2024-05-12 04:34:00 +0530

    refactor: Update Modal component to include form for task editing

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,44 +1,44 @@
 import { useState } from "react";
 
-function Modal({mode,setShowModal,task}) {
+function Modal({ mode, setShowModal, task }) {
   const editMode = mode === "edit" ? true : false;
   const [data, setData] = useState({
-    user_email: editMode? task.user_email:null,
-    title: editMode? task.progress:null,
-    progress: editMode? task.progress:50,
+    user_email: editMode ? task.user_email : null,
+    title: editMode ? task.progress : null,
+    progress: editMode ? task.progress : 50,
     date: editMode ? "" : new Date(),
   });
 
-  const postData = () => {
+  const postData = async () => {
     try {
-      fetch('http://localhost:8000/todos/', {
-        'method': 'POST',
-        'headers': {
-          'Content-Type': 'application/json',
+      const response = await fetch("http://localhost:8000/todos/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        'body': JSON.stringify(data),
+        body: JSON.stringify(data),
       });
+      console.log(response);
     } catch (err) {
       console.error(err);
     }
   };
 
   const hanlechange = (e) => {
-    
-    const {name,value}=e.target
-    setData(data =>({
+    const { name, value } = e.target;
+    setData((data) => ({
       ...data,
-      [name]:value
-      }))
+      [name]: value,
+    }));
 
-      console.log(data)
+    console.log(data);
   };
   return (
     <div className="overlay">
       <div className="modal">
         <div className="form-title-container">
           <h3>Let's {mode} your task</h3>
-          <button onClick={()=> setShowModal(false)}>X</button>
+          <button onClick={() => setShowModal(false)}>X</button>
         </div>
         <form>
           <input
@@ -61,7 +61,7 @@ function Modal({mode,setShowModal,task}) {
             value={data.progress}
             onChange={hanlechange}
           />
-          <input className={mode} type="submit" />
+          <input className={mode} type="submit" onClick={editMode ? '':postData} />
         </form>
       </div>
     </div>
