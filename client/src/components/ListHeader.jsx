3c882310f59a7e24b1a8ce934a8799a2298dbecb
Tridib11 <tridibghosh12345@gmail.com>
2024-05-12 02:47:27 +0530

    Files added

--- client/src/components/ListHeader.jsx
+++ client/src/components/ListHeader.jsx
@@ -1,17 +1,19 @@
 // eslint-disable-next-line react/prop-types
 function ListHeader({ listName }) {
-    function signOut(){
-        console.log("Sign out")
-    }
-    return (
-        <div className="list-header">
-            <h1>{listName}</h1>
-            <div className="button-container">
-                <button className="create">ADD NEW</button>
-                <button className="signout" onClick={signOut}>SIGN OUT</button>
-            </div>
-        </div>
-    )
+  function signOut() {
+    console.log("Sign out");
+  }
+  return (
+    <div className="list-header">
+      <h1>{listName}</h1>
+      <div className="button-container">
+        <button className="create">ADD NEW</button>
+        <button className="signout" onClick={signOut}>
+          SIGN OUT
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default ListHeader
+export default ListHeader;
