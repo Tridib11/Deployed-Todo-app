75bbfd859c98d48e2605f80b9de516b20d287582
Tridib11 <tridibghosh12345@gmail.com>
2024-05-12 05:20:38 +0530

    refactor: Include express.json middleware in server.js for parsing JSON data

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const app=express()
 const pool=require('./db')
 
 app.use(cors())
+app.use(express.json())
 
 
 //get all todos
@@ -25,11 +26,13 @@ app.get('/todos/:userEmail',async(req,res)=>{
 
 //Create a new Todo
 
-app.post('todos/',(req,res)=>{
+app.post('/todos',async(req,res)=>{
   const {user_email,title,progress,date}=req.body
+  console.log(user_email,title,progress,date)
   const id=uuidv4()
   try{
-    pool.query(`INSERT INTO todos (id,user_email,title,progress,date) VALUES($1,$2,$3,$4,$5)`,[id,user_email,title,progress,date])
+    const newTodo=await pool.query(`INSERT INTO todos (id,user_email,title,progress,date) VALUES($1,$2,$3,$4,$5)`,[id,user_email,title,progress,date])
+    res.json(newTodo)
   }
   catch(err){
     console.error(err)
