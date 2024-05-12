b92dcb912b9c5df6267e31351e60478d463074e7
Tridib11 <tridibghosh12345@gmail.com>
2024-05-12 12:07:49 +0530

    refactor: Update Auth component to include email and password fields for login and signup

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,15 +1,17 @@
 function Auth() {
-  const isLogin=false
-  return <div className="auth-container">
-    <div className="auth-container-box">
+  const isLogin = false;
+  return (
+    <div className="auth-container">
+      <div className="auth-container-box">
         <form>
-          <h2>{isLogin ? 'Please login':'Please Signup'}</h2>
-          <input/>
-          <input/>
-          <input/>
+          <h2>{isLogin ? "Please login" : "Please Signup"}</h2>
+          <input type="email" placeholder="email"/>
+          <input type="password" placeholder="password"/>
+          {!isLogin && <input type="password" placeholder="confirm-password"/>}
         </form>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default Auth;
