111264c0875bad44c425e7c91efe98f219389120
Tridib11 <tridibghosh12345@gmail.com>
2024-05-12 12:25:43 +0530

    refactor: Update Auth component to include error state and form submission buttons

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 function Auth() {
+  const[error,setError]=useState(null);
   const isLogin = false;
   return (
     <div className="auth-container">
@@ -8,7 +11,13 @@ function Auth() {
           <input type="email" placeholder="email"/>
           <input type="password" placeholder="password"/>
           {!isLogin && <input type="password" placeholder="confirm-password"/>}
+          <input type="submit" className="create"/>
+          {error && <p>{error}</p>}
         </form>
+        <div className="auth-options">
+          <button>Sign up</button>
+          <button>Login</button>
+        </div>
       </div>
     </div>
   );
