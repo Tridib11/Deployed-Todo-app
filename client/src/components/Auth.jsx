11d0a1379abc252ca2cac0b57d2a547d2f7f985f
Tridib11 <tridibghosh12345@gmail.com>
2024-05-12 12:05:11 +0530

    refactor: Update Auth component to display login or signup form based on isLogin variable

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,8 +1,9 @@
 function Auth() {
+  const isLogin=false
   return <div className="auth-container">
     <div className="auth-container-box">
         <form>
-          <h2></h2>
+          <h2>{isLogin ? 'Please login':'Please Signup'}</h2>
           <input/>
           <input/>
           <input/>
