6b44d5a37bf485c43323450205924e30a7546080
Tridib11 <tridibghosh12345@gmail.com>
2024-05-12 12:01:47 +0530

    refactor: Include form for user authentication in Auth component

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,5 +1,14 @@
 function Auth() {
-  return <div>Tridib</div>;
+  return <div className="auth-container">
+    <div className="auth-container-box">
+        <form>
+          <h2></h2>
+          <input/>
+          <input/>
+          <input/>
+        </form>
+    </div>
+  </div>;
 }
 
 export default Auth;
