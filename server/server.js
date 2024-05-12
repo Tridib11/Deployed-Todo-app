4389f69e251ae41b8091a59bcaabfaa5a228ce65
Tridib11 <tridibghosh12345@gmail.com>
2024-05-12 05:32:47 +0530

    refactor: Update server.js to include express.json middleware for parsing JSON data

--- server/server.js
+++ server/server.js
@@ -1,45 +1,61 @@
-const PORT= process.env.PORT ??  8000;
-const cors=require('cors')
-const {v4:uuidv4}=require('uuid')
-const express=require("express")
-const app=express()
+const PORT = process.env.PORT ?? 8000;
+const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
+const express = require("express");
+const app = express();
 
-const pool=require('./db')
+const pool = require("./db");
 
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 //get all todos
 
-app.get('/todos/:userEmail',async(req,res)=>{
-
-  const { userEmail }=req.params
-  console.log(userEmail)
-  try{
-    const todos=await pool.query('SELECT * FROM todos WHERE user_email =$1',[userEmail])
-    res.json(todos.rows)
-  }catch(err){
-    console.error(err)
+app.get("/todos/:userEmail", async (req, res) => {
+  const { userEmail } = req.params;
+  console.log(userEmail);
+  try {
+    const todos = await pool.query("SELECT * FROM todos WHERE user_email =$1", [
+      userEmail,
+    ]);
+    res.json(todos.rows);
+  } catch (err) {
+    console.error(err);
   }
-})
+});
 
 //Create a new Todo
 
-app.post('/todos',async(req,res)=>{
-  const {user_email,title,progress,date}=req.body
-  console.log(user_email,title,progress,date)
-  const id=uuidv4()
-  try{
-    const newTodo=await pool.query(`INSERT INTO todos (id,user_email,title,progress,date) VALUES($1,$2,$3,$4,$5)`,[id,user_email,title,progress,date])
-    res.json(newTodo)
+app.post("/todos", async (req, res) => {
+  const { user_email, title, progress, date } = req.body;
+  console.log(user_email, title, progress, date);
+  const id = uuidv4();
+  try {
+    const newTodo = await pool.query(
+      `INSERT INTO todos (id,user_email,title,progress,date) VALUES($1,$2,$3,$4,$5)`,
+      [id, user_email, title, progress, date]
+    );
+    res.json(newTodo);
+  } catch (err) {
+    console.error(err);
   }
-  catch(err){
-    console.error(err)
+});
+
+//edit todo
+
+app.put("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  const { user_email,title, progress, date } = req.body;
+  try {
+    const updateTodo = await pool.query(
+      `UPDATE todos SET title=$1,progress=$2,date=$3 WHERE id=$4`,
+      [title, progress, date, id]
+    );
+    res.json(updateTodo);
+  } catch (err) {
+    console.error(err);
   }
-})
-
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
